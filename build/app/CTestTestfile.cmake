0fed2cfae7a67c3efcc10acd019a88528f86e3b5
tarunreddyy <tarunsaireddy1998@gmail.com>
2023-10-08 18:05:39 -0400

    Added PIDController::compute function and other modifications

---
+++ build/app/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tarun/Masters/ENPM808X/PID_Test_Driven_Development/app
+# Build directory: /home/tarun/Masters/ENPM808X/PID_Test_Driven_Development/build/app
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/libs/pid/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tarun/Masters/ENPM808X/PID_Test_Driven_Development/libs/pid
+# Build directory: /home/tarun/Masters/ENPM808X/PID_Test_Driven_Development/build/libs/pid
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
