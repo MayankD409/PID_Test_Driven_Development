8d8ee3fe6b1b2d7c168dbd138df6120c7b0b70aa
Mayank Deshpande <deshpandemayank5@gmail.com>
2023-10-09 22:35:36 -0400

    Added cppcheck and cpplint results

--- test/test.cpp
+++ test/test.cpp
@@ -1,39 +1,40 @@
 #include <gtest/gtest.h>
+
 #include "pid.hpp"
 
 TEST(PIDControllerTest, ComputeTest) {
-    // Initialize a PID controller with specific gains
-    PIDController pidController(0.1, 0.01, 0.5);
+  // Initialize a PID controller with specific gains
+  PIDController pidController(0.1, 0.01, 0.5);
 
-    // Set a target setpoint and actual velocity
-    double setpoint = 100.0;
-    double actualVelocity = 50.0;
+  // Set a target setpoint and actual velocity
+  double setpoint = 100.0;
+  double actualVelocity = 50.0;
 
-    // Call the compute method to calculate new velocity
-    double newVelocity = pidController.compute(setpoint, actualVelocity);
+  // Call the compute method to calculate new velocity
+  double newVelocity = pidController.compute(setpoint, actualVelocity);
 
-    double expectedValue = 30.5;
-    double tolerance = 0.01;    // Tolerance for floating-point comparison
+  double expectedValue = 30.5;
+  double tolerance = 0.01;  // Tolerance for floating-point comparison
 
-    EXPECT_NEAR(newVelocity, expectedValue, tolerance);
+  EXPECT_NEAR(newVelocity, expectedValue, tolerance);
 }
 
 TEST(PIDControllerTest, SetAndGetGainsTest) {
-    // Initialize a PID controller with default gains
-    PIDController pidController(0.0, 0.0, 0.0);
+  // Initialize a PID controller with default gains
+  PIDController pidController(0.0, 0.0, 0.0);
 
-    // Set new gains using setter methods
-    pidController.setKp(0.2);
-    pidController.setKi(0.02);
-    pidController.setKd(0.8);
+  // Set new gains using setter methods
+  pidController.setKp(0.2);
+  pidController.setKi(0.02);
+  pidController.setKd(0.8);
 
-    // Get the gains using getter methods
-    double kp = pidController.getKp();
-    double ki = pidController.getKi();
-    double kd = pidController.getKd();
+  // Get the gains using getter methods
+  double kp = pidController.getKp();
+  double ki = pidController.getKi();
+  double kd = pidController.getKd();
 
-    // Assert that the retrieved gains match the set values
-    EXPECT_DOUBLE_EQ(kp, 0.2);
-    EXPECT_DOUBLE_EQ(ki, 0.02);
-    EXPECT_DOUBLE_EQ(kd, 0.8);
+  // Assert that the retrieved gains match the set values
+  EXPECT_DOUBLE_EQ(kp, 0.2);
+  EXPECT_DOUBLE_EQ(ki, 0.02);
+  EXPECT_DOUBLE_EQ(kd, 0.8);
 }
